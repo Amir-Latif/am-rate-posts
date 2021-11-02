76a940f5b338581ed56910ec528105c3cc0fac4d
Amir Latif <amirwesa@gmail.com>
2021-11-02 14:11:01 +0200

    removed license details

--- am-rate-posts.php
+++ am-rate-posts.php
@@ -2,15 +2,13 @@
 
 /**
  * Plugin Name: AM - Rate Posts
- * Plugin URI: https://amir-latif.github.io/portfolio/wp-plugins/amrp
+ * Plugin URI: https://amir-latif.github.io/portfolio/amrp
  * Description: Adds rating functionality to posts
  * Version: 1.0
  * Requires at least: 5.2
  * Requires PHP: 7.2
  * Author: Amir Latif
  * Author URI: https://amir-latif.github.io/portfolio/
- * License: GPL v2 or later
- * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: WordPress Plugins
  */
 ?>
